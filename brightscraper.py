b1cba8bba0b05db610ce7729fb90a2f29637e933
Samuel Cloward <u0342268@ad.utah.edu>
2024-08-22 15:14:36 -0600

    Added activity extract functions

--- brightscraper.py
+++ brightscraper.py
@@ -1,27 +1,33 @@
+from dataclasses import dataclass
 from datetime import datetime
+import json
 import logging
 import os
 import re
 import time
 from typing import Optional
+from urllib.request import urlopen
 from zoneinfo import ZoneInfo
 import yaml
 import argparse
 import random
 import sys
 
-import requests
 import undetected_chromedriver as uc
 
-from selenium.common.exceptions import ElementNotVisibleException
+from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
+
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 
 from webdriver_manager.chrome import ChromeDriverManager
+
+from PIL import Image
+
+import piexif
 
 """I was saving pictures ad hoc through the brightwheel app, but got way behind
 and didn't want to lose them if my kid changed schools or lost access to the app.
@@ -29,7 +35,7 @@ This uses selenium to crawl a BrightWheel (https://mybrightwheel.com/) profile
 for images, find all of them, pass the cookies to requests, and then download
 all images in bulk. Works with current site design as off 6/24/19"""
 
-UTC = ZoneInfo("UTC")
+BASE_BRIGHTWHEEL_URL = "https://schools.mybrightwheel.com"
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -46,20 +52,24 @@ console_handler.setLevel(logging.INFO)
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
+@dataclass
+class Student:
+    id: str
+    name: str
 class Config(yaml.YAMLObject):
     yaml_tag = u'!Config'
     yaml_loader=yaml.SafeLoader
     username:  str
     password: str
     guardian_id: str | None
-    child_ids: list[str] | None
+    child_ids: list[Student] | None
     signin_url: str
     kidlist_url: str
     start_date: datetime
     end_date: datetime
     start_page:int | None
     page_size:int | None
-    timezone: str | None
+    timezone: ZoneInfo | None
 
     def __init__(
             self, 
@@ -85,7 +95,7 @@ class Config(yaml.YAMLObject):
         self.end_date = datetime.strptime(enddate, "%m/%d/%Y")
         self.start_page = startpage
         self.page_size = pagesize
-        self.timezone = timezone
+        self.timezone = ZoneInfo(timezone)
 
 def config_parser() -> Config:
     """parse config file in config.yml if present"""
@@ -101,8 +111,6 @@ def config_parser() -> Config:
     except KeyError:
         logger.error("[!] - Check config file, missing required values")
         raise SystemExit
-
-    return config
 
 
 def get_random_time():
@@ -136,142 +144,73 @@ def signme_in(browser, config: Config):
     time.sleep(get_random_time() * 3)  # Change this to the amount of time you need to solve the captcha manually
     return browser
 
-
-def pic_finder(browser, kidlist_url, startdate, enddate, args):
-    """This is the core logic of the script, navigate through the site, find
-    the page with photos, scroll to the bottom to load them all, load them all
-    in a specified date range, and create an iterable list of image URLs"""
-
-    browser.get(kidlist_url)
-
-    time.sleep(get_random_time())
-
-    # This xpath is generic enough to find any student listed.
-    # You need to iterate through a list you create if you have more than one
+def get_json_from_session(session: webdriver.Chrome) -> dict:    
     try:
-        students = browser.find_elements(By.XPATH, "//a[contains(@href, '/students/')]")
-        logger.info("got students")
-        if not students:
-            raise Exception("No student URLs found")
-        if args.student_number:
-            profile_url = students[args.student_number - 1].get_property("href")
-        else:
-            try:
-                logger.info("Select a student:")
-                for i, student in enumerate(students):
-                    logger.info(f"{i}. {student.text}")
-                selection = int(input("Enter a number: "))
-                profile_url = students[selection].get_property("href")
-                # Replace last part of url path with feed
-            except Exception as e:
-                logger.error(f"[!] - Unable to find student: {e}")
-                raise SystemExit
-
-    except Exception as e:
-        logger.error(f"[!] - Unable to find profile page: {e}")
+        json_container = session.find_element(By.TAG_NAME, 'pre')
+        parsed_json = json.loads(json_container.text)
+    except NoSuchElementException:
+        logger.error("[!] - 'pre' element not found in response")
         raise SystemExit
+    return parsed_json
 
-    feed_url = profile_url.rsplit("/", 1)[0] + "/feed"
+def get_guardian_id(session: webdriver.Chrome, url: str = "/api/v1/users/me"):
+    me_url = f"{BASE_BRIGHTWHEEL_URL}{url}"
+    session.get(me_url)
+    me = get_json_from_session(session=session)
+    logger.info('me %s', me)
+    guardian_id: str = me.get("object_id", None)
+    if not guardian_id:
+        logger.error("[!] - could not extract guardian id")
+        raise SystemExit
+    return guardian_id
 
-    time.sleep(get_random_time())
+def get_child_ids(session: webdriver.Chrome, id: str, index: Optional[int]):
+    students_url = f"{BASE_BRIGHTWHEEL_URL}/api/v1/guardians/{id}/students?include[]=schools"
+    session.get(students_url)
+    students = get_json_from_session(session=session)
+    logger.info('children %s', students)
+    students_list: list[str] = students.get("students", [])
+    if not students_list:
+        logger.error("[!] - could not extract student ids")
+        raise SystemExit
+    if index:
+        students_list = [students_list[index]]
 
-    # Get to feed, this is where the pictures are
-    # Open the feed_url
-    browser.get(feed_url)
-    time.sleep(3)
+    return [Student(id = student["student"]["object_id"], name = student["student"]["first_name"]) for student in students_list]
 
-    # Populate the selector for date range to load all images
-    start_date = browser.find_element(By.NAME, "start_date")
-    start_date.send_keys(startdate)
-    end_date = browser.find_element(By.NAME, "end_date")
-    end_date.send_keys(enddate)
-    select = browser.find_element(By.ID, "select-input-2")
-    select.send_keys("Photo")
-    select.send_keys(Keys.ENTER)
+def get_activities(session: webdriver.Chrome, id: str, page: int, page_size: int, start: str, end: str,):
+    activities_url = f"{BASE_BRIGHTWHEEL_URL}/api/v1/students/{id}/activities?page={page}&page_size={page_size}&start_date={start}&end_date={end}&action_type=ac_photo&include_parent_actions=true"
+    session.get(activities_url)
+    activities = get_json_from_session(session=session)
+    logger.info('activities %s', activities)
+    activity_list: list[dict] = activities.get("activities", None)
+    if not activity_list:
+        logger.error("[!] - could not extract activities")
+        raise SystemExit
+    return activity_list
 
-    # This is the XPATH for the Apply button.
-    browser.find_element(By.XPATH, '//*[@id="main"]/div/div/div[2]/div/form/button').click()
-    logger.info("[-] - Applied date range")
-    try:
-        last_height = browser.execute_script("return document.body.scrollHeight")
-        counter = 0
-        state = True
-        while state is True:
-            try:
-                button = WebDriverWait(browser, 7).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Load more"]')))
-                button.click()
-            except:
-                if counter == 1:
-                    logger.info("[-] No Loading button found, there may not be many pictures")
-                else:
-                    logger.error("[!] No loading button found")
-            browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            counter += 1
-
-            # Wait to load the page.
-            time.sleep(2)
-
-            # Calculate new scroll height and compare with last scroll height.
-            new_height = browser.execute_script("return document.body.scrollHeight")
-
-            if new_height == last_height:
-                logger.info("[!] Page fully loaded, finding images...")
-                state = False
-
-            last_height = new_height
-
-    except ElementNotVisibleException:
-        logger.info("none")
-
-
-    matches = re.findall(r'<img\s+src="([^"]+)"', browser.page_source,)
-    count_matches = len(matches)
-    if count_matches == 0:
-        logger.error("[!] No Images found to download! Check the source target page")
-    else:
-        logger.info("[!] Found {} files to download...".format(count_matches))
-
-    return browser, matches
-
-
-def get_images(browser, matches):
-    """Brightwheel moved their image storage to a CDN with signed urls, so we dont need to use
-    cookies anymore. I'm eaving this code in case that behavior ever comes back
-    
-    Since Selenium doesn't handle saving images well, requests
-    can do this for us, but we need to pass it the cookies"""
-    # Check if the ./pics/ directory exists, create it if it doesn't
-    if not os.path.exists('./pics/'):
-        os.makedirs('./pics/')
-
-    # cookies = browser.get_cookies()
-    # session = requests.Session()
-    # for cookie in cookies:
-    #     session.cookies.set(cookie["name"], cookie["value"])
-    # for match in matches:
-    #     try:
-    #         filename = match.split("/")[-1].split("?")[0].split("%2F")[-1]
-    #         request = session.get(match)
-    #         open("./pics/" + filename, "wb").write(request.content)
-    #         logger.info("[-] - Downloading {}".format(filename))
-    #     except:
-    #         logger.error("[!] - Failed to save {}".format(match))
-    # try:
-    #     session.cookies.clear()
-    #     browser.delete_all_cookies()
-    #     logger.info("[-] - Cleared cookies")
-    # except:
-    #     logger.error("[!] - Failed to clear cookies")
-
-    for match in matches:
-        try:
-            filename = match.split("/")[-1].split("?")[0].split("%2F")[-1]
-            request = requests.get(match)
-            open("./pics/" + filename, "wb").write(request.content)
-            logger.info("[-] - Downloading {}".format(filename))
-        except:
-            logger.error("[!] - Failed to save {}".format(match))
-
+def generate_exif_data(activity: dict, config: Config):    
+    created_date_str = activity["created_at"]        
+    created_date = datetime.strptime(created_date_str, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=config.timezone)    
+    formatted_created_date = created_date.astimezone(tz=config.timezone).strftime('%Y:%m:%d %H:%M:%S')    
+    actor: dict = activity["actor"]
+    teacher_name: str = ""
+    if actor:
+        first_name: str = actor.get("first_name", "")
+        last_name: str = actor.get("last_name", "")
+        teacher_name: str = f"{first_name} {last_name}"
+    note: str = activity.get("note", "")    
+    zeroth_ifd = {
+        piexif.ImageIFD.Artist: teacher_name.encode("utf-8", errors="ignore") if teacher_name else "",
+        piexif.ImageIFD.ImageDescription: note.encode("utf-8", errors="ignore") if note else "",
+        piexif.ImageIFD.DateTime: formatted_created_date,
+    }
+    exif_ifd = {
+        piexif.ExifIFD.DateTimeOriginal: formatted_created_date,
+    }
+    exif_dict = {"0th": zeroth_ifd, "Exif": exif_ifd}
+    exif_bytes = piexif.dump(exif_dict)
+    return exif_bytes
 
 def use_chrome_selenium(version: Optional[int] = 127) -> uc.Chrome:
     """Use undetected chrome driver
@@ -309,7 +248,7 @@ def use_existing_chrome_session():
         sys.exit(1)
 
 
-def main():
+def main():   
     parser = argparse.ArgumentParser(description="Brightwheel Scraper")
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-c", "--chrome_selenium", action="store_true", help="Use existing Chrome session to login to Brightwheel")
@@ -326,14 +265,48 @@ def main():
     else:
         logger.error("[!] - No browser selected, exiting")
 
+     # Check if the ./pics/ directory exists, create it if it doesn't
+    if not os.path.exists('./pics/'):
+        os.makedirs('./pics/')
+        
     config = config_parser()
 
     session = signme_in(browser, config=config)
 
-    session, matches = pic_finder(session, kidlist_url, startdate, enddate, args)
+    if not config.guardian_id:
+        config.guardian_id=get_guardian_id(session)
 
-    get_images(session, matches)
+    if not config.child_ids:
+        config.child_ids = get_child_ids(session)
 
+    start_date_tz = config.start_date.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    end_date_tz = config.end_date.strftime("%Y-%m-%dT%H:%M:%S.%fZ")    
+    # Begin fetching and saving images for each child
+    for child in config.child_ids:
+        page = config.start_page
+        while True:
+            activities = get_activities(
+                session=session, 
+                id=child.id, 
+                page=page, 
+                page_size=config.page_size, 
+                start=start_date_tz, 
+                end=end_date_tz
+            )
+            for activity in activities:
+
+                event_date_str = activity["event_date"]                
+                event_date = datetime.strptime(event_date_str, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=config.timezone)    
+                formatted_event_date = event_date.astimezone(tz=config.timezone).strftime("%Y%m%d")
+                
+                media = activity["media"]
+
+                exif = generate_exif_data(activity, config=config)           
+
+                filename = f"{child.name}_{formatted_event_date}_{media['object_id']}.jpg"
+
+                with Image.open(urlopen(media["image_url"])) as img:
+                    img.save("./pics/" + filename, exif=exif)
 
 if __name__ == "__main__":
     main()
